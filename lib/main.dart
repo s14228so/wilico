0b4f8bd687273441d10a915329221979d93335d1
satoshi <saongtx7@gmail.com>
2019-09-18 01:34:26 +0900

    firebase通信確認plan一覧の取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wilico/pages/mypage.dart';
 import './plan_detail.dart';
 import "models/plan.dart";
 import 'online.dart';
@@ -36,6 +37,30 @@ class _MyHomePageState extends State<MyHomePage> {
               )),
           centerTitle: true,
           leading: Icon(Icons.search),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('プランを探す'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.timeline),
+              title: Text('タイムライン'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              title: Text('トーク'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('マイページ'),
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Colors.amber[800],
+          onTap: null,
         ),
         body: Column(
           children: <Widget>[
@@ -90,7 +115,10 @@ class _MyHomePageState extends State<MyHomePage> {
               title: Text('マイページ'),
               trailing: Icon(Icons.arrow_forward),
               onTap: () {
-                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyPage()),
+                );
               },
             ),
             ListTile(
@@ -111,113 +139,126 @@ class _MyHomePageState extends State<MyHomePage> {
         )));
   }
 
-  Widget createListView() {
-    var size = MediaQuery.of(context).size;
-    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
-    final double itemWidth = size.width / 2;
-
-    final String img =
-        "https://firebasestorage.googleapis.com/v0/b/coach-59f10.appspot.com/o/images%2FIMG_5557.JPG?alt=media&token=88c67509-37cb-4457-8871-55b9a91a0963";
-    return Container(
-      padding: EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
-      child: GridView.count(
-        mainAxisSpacing: 20,
-        childAspectRatio: 0.7,
-        crossAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: List.generate(8, (index) {
-          return Container(
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        PlanDetail(plan: new Plan((index + 1).toString())),
-                  ),
-                );
-              },
-              child: Card(
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      new Container(
-                          width: 100.0,
-                          height: 100.0,
-                          margin: EdgeInsets.only(top: 20.0),
-                          decoration: new BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                width: 2,
-                                color: Color.fromRGBO(37, 178, 144, 1),
-                              ),
-                              image: new DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: new NetworkImage(img)))),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "冨迫孔聡コーチ",
-                        style: TextStyle(
-                            fontSize: 14.0, fontWeight: FontWeight.w700),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "おちんちんコース ${index + 1}",
-                          style: TextStyle(
-                              fontSize: 14.0, fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("${(index + 1) * 1000}円/月")),
-                      Wrap(
-                        // alignment: WrapAlignment.start,
-                        spacing: 2.0, // gap between adjacent chips
-                        direction: Axis.horizontal, // gap between lines
-                        children: <Widget>[
-                          Container(
-                            child: Chip(
-                              label: Text(
-                                'Hamilton',
-                                style: TextStyle(
-                                  fontSize: 10.0,
-                                  color: Color.fromRGBO(37, 178, 144, 1),
+  createListView() {
+    return StreamBuilder(
+        stream: Firestore.instance.collection('plans').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          // エラーの場合
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading ...');
+            default:
+              return Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
+                  child: GridView.count(
+                      childAspectRatio: 0.7,
+                      crossAxisSpacing: 10,
+                      crossAxisCount: 2,
+                      children: List.generate(snapshot.data.documents.length,
+                          (index) {
+                        return Container(
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PlanDetail(
+                                      plan: new Plan((index + 1).toString())),
+                                ),
+                              );
+                            },
+                            child: Card(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    new Container(
+                                        width: 100.0,
+                                        height: 100.0,
+                                        margin: EdgeInsets.only(top: 20.0),
+                                        decoration: new BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                              width: 2,
+                                              color: Color.fromRGBO(
+                                                  37, 178, 144, 1),
+                                            ),
+                                            image: new DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: new NetworkImage("")))),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Text(
+                                      snapshot.data.documents[index]["title"],
+                                      style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        "おちんちんコース ${index + 1}",
+                                        style: TextStyle(
+                                            fontSize: 14.0,
+                                            fontWeight: FontWeight.w700),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Align(
+                                        alignment: Alignment.centerLeft,
+                                        child:
+                                            Text("${(index + 1) * 1000}円/月")),
+                                    Wrap(
+                                      // alignment: WrapAlignment.start,
+                                      spacing:
+                                          2.0, // gap between adjacent chips
+                                      direction:
+                                          Axis.horizontal, // gap between lines
+                                      children: <Widget>[
+                                        Container(
+                                          child: Chip(
+                                            label: Text(
+                                              'Hamilton',
+                                              style: TextStyle(
+                                                fontSize: 10.0,
+                                                color: Color.fromRGBO(
+                                                    37, 178, 144, 1),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          child: Chip(
+                                            label: Text(
+                                              'Hamilton',
+                                              style: TextStyle(
+                                                fontSize: 10.0,
+                                                color: Color.fromRGBO(
+                                                    37, 178, 144, 1),
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
                                 ),
                               ),
                             ),
                           ),
-                          Container(
-                            child: Chip(
-                              label: Text(
-                                'Hamilton',
-                                style: TextStyle(
-                                  fontSize: 10.0,
-                                  color: Color.fromRGBO(37, 178, 144, 1),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
-        }),
-      ),
-    );
+                        );
+                      })));
+          }
+        });
   }
 }
